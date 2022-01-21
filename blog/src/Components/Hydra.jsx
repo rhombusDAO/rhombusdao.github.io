fa7eef88a5c6e6a8193d83f59ef3e014bbd01a5d
Justin Kuhn <justintkuhn@gmail.com>
2022-01-21 15:07:57 +0000

    added spacing and text

--- blog/src/Components/Hydra.jsx
+++ blog/src/Components/Hydra.jsx
@@ -3,21 +3,45 @@ import Hydra from 'hydra-synth';
 
 const HydraCanvas = () =>  {
 
-
-
          useEffect(() => {
 
             const hydra = new Hydra({ 
                 detectAudio: false,
+                canvas: document.querySelector('#myCanvas')
              });
 
-             osc().out();
+
+             setFunction({
+                name: 'rhombify',
+                type: 'coord',
+                inputs: [
+                  {
+                    type: 'float',
+                    name: 'amt',
+                    default: '0.5'
+                  }
+                ],
+                glsl:
+              `vec2 xy = _st - vec2(0.5);
+              xy.x += amt*xy.y;
+              xy.y += amt*xy.y;
+              xy += 0.5;
+              return xy;`
+              })
+             //returns an error, but still works
+             src(o0).blend(osc(10, 0.05, 1), 0.05)
+             .mask(shape(4, 0.6).scale(1, 0.8).rhombify(0.25))
+             .modulate(noise(8, 0.01), 0.001)
+             .out(o0)
+             render(o0)
+
+
             console.log('mounted');
             return () => console.log('unmounting...');
           }, [])  // <-- add this empty array here
 
         return (
-                <canvas id="hydraCanvas"></canvas>
+                <canvas id="myCanvas"></canvas>
         )
     
 }
